6a3cbbd944635afdcdba21c3d8523e8dcb786748
Prathamesh-Ghatole <prathamesh.s.ghatole@gmail.com>
2022-06-21 08:42:06 +0000

    wrote and tested lib/postgres_conn.py

--- lib/postgres_conn.py
+++ lib/postgres_conn.py
@@ -9,11 +9,5 @@ conn = psycopg2.connect(
 
 cursor = conn.cursor()
 
-def check_in_recording(rec_mbid):
-    cursor.execute("SELECT gid FROM recording WHERE gid=%(id)s;", {'id':rec_mbid})
-    fetch = cursor.fetchone()
-    
-    if type(fetch) == tuple:
-        return fetch[0]
-    else:
-        return None
+get_con = lambda : conn
+get_cursror = lambda : cursor
