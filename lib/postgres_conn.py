378731a6dc70b58a54f811534aaf6dd2c0990532
Prathamesh-Ghatole <prathamesh.s.ghatole@gmail.com>
2022-07-15 07:31:11 +0000

    replaced SQLalchemy connector with psycopg2

--- lib/postgres_conn.py
+++ lib/postgres_conn.py
@@ -1,7 +1,17 @@
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://musicbrainz:musicbrainz@localhost/musicbrainz_db')
+# engine = create_engine('postgresql://musicbrainz:musicbrainz@localhost/musicbrainz_db')
 
-# conn = engine.connect()
-def get_con(): 
-    return engine.connect()
+# # conn = engine.connect()
+# def get_con(): 
+#     return engine.connect()
+
+from psycopg2 import connect
+def get_con():
+    conn = connect(
+        host="localhost",
+        database="musicbrainz_db",
+        user="musicbrainz",
+        port=5432)
+
+    return conn
