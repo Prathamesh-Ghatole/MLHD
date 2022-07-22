f683ff901507b4da8d51eb1795b46926733c39f5
Prathamesh-Ghatole <prathamesh.s.ghatole@gmail.com>
2022-07-22 10:35:30 +0000

    added get_artist_credit, refactored existing extra long queries

--- lib/mb.py
+++ lib/mb.py
@@ -23,10 +23,18 @@ def get_recording_name(limit=None):
     return get_table('SELECT gid, name FROM recording', limit)
 
 def get_recording_redirects(limit=None):
-    return get_table('select r.gid AS new, rgr.gid AS old from recording r join recording_gid_redirect rgr on rgr.new_id = r.id', limit)
+    return get_table("""
+    select r.gid AS new, 
+    rgr.gid AS old 
+    from recording r 
+    join recording_gid_redirect rgr 
+    on rgr.new_id = r.id""", limit)
 
 def get_recording_canonical(limit=None): 
-    return get_table('select recording_mbid as old, canonical_recording_mbid as new from mapping.canonical_recording_redirect', limit)
+    return get_table("""
+    select recording_mbid as old, 
+    canonical_recording_mbid as new 
+    from mapping.canonical_recording_redirect""", limit)
 ### Artists
 
 def get_artist(limit=None): 
@@ -36,11 +44,19 @@ def get_artist_name(limit=None):
     return get_table('SELECT gid, name FROM artist', limit)
 
 def get_artist_redirects(limit=None):
-    return get_table('select a.gid AS new, agr.gid AS old from artist a join artist_gid_redirect agr on agr.new_id = a.id', limit)
+    return get_table("""
+    select a.gid AS new, 
+    agr.gid AS old from artist a 
+    join artist_gid_redirect agr 
+    on agr.new_id = a.id""", limit)
 
 ### Tracks
 def get_track_redirects(limit=None): 
-    return get_table('select t.gid AS new, tgr.gid AS old from track t join track_gid_redirect tgr on tgr.new_id = t.id', limit)
+    return get_table("""
+    select t.gid AS new, 
+    tgr.gid AS old from track t 
+    join track_gid_redirect tgr 
+    on tgr.new_id = t.id""", limit)
 
 def get_track_redirects_old(limit=None): 
     return get_table('select gid from track_gid_redirect', limit)
@@ -50,3 +66,11 @@ def get_tracks(limit=None):
 
 def get_track_name(limit=None): 
     return get_table('SELECT gid, name FROM track', limit)
+
+def get_artist_credit(limit=None): 
+    return get_table("""
+    select recording.gid as rec_gid,
+    recording.name as rec_name,
+    ac.name as artist_credit 
+    from recording join artist_credit ac 
+    on ac.id=artist_credit;""", limit)
