ba71ec015a8647cb1acef39820d467e2d2cf5431
Prathamesh-Ghatole <prathamesh.s.ghatole@gmail.com>
2022-10-30 18:58:43 +0000

    Specify num of files to clean, RESET_LOG_BEFORE_RUN; Added notes to include trailing "/" in path string. Added PICKLE_PATH for backwards compatibility Updated default LOG_EPOCH to 1 Updated LOG_WRITE_PATH to LOG_WRITE_ROOT

--- clean_master_config.py
+++ clean_master_config.py
@@ -1,7 +1,13 @@
 # Settings for the clean_master.py script
 
+# Total mumber of files to clean (use for debugging). Set to None to process all.
+HOW_MANY = 5
+
 # Maximum number of workers for concurrent.futures.ThreadPoolExecutor
 MAX_WORKERS = 5
+
+# Batch size for multiprocessing
+CHUNK_SIZE = 5
 
 # When TRUE -> Keep missing values in the data. (maintains the original structure/shape of MLHD)
 KEEP_MISSING = True
@@ -12,4 +18,5 @@ TURN_BLANK = True
 ## Export options
 WRITE_TSV_ZSTD = True
 WRITE_PARQUET = False
-LOG_FILE_NAME = "clean_master_log.json" # Name of the log file
+LOG_FILE_NAME = "clean_master_log.csv"  # Name of the log file
+RESET_LOG_BEFORE_RUN = True  # When TRUE -> Reset the log file before running the script
